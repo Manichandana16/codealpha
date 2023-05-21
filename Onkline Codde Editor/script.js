97947430a15533b780f9d3bf4994779e52fe9b5d
ch. Mani chandana <99012242+Manichandana16@users.noreply.github.com>
2023-05-21 15:17:27 +0530

    Update script.js

--- Onkline Codde Editor/script.js
+++ Onkline Codde Editor/script.js
@@ -1,9 +1,25 @@
-function ans(){
-    let html = document.getElementById("html-code").value;
-    let css = document.getElementById("css-code").value;
-    let js = document.getElementById("js-code").value;
-    let output = document.getElementById("output");
+const html_code = document.querySelector('.html-code textarea');
+const css_code = document.querySelector('.css-code textarea');
+const js_code = document.querySelector('.js-code textarea');
+const result = document.querySelector('#result');
 
-    output.contentDocument.body.innerHTML = html + "<style>"+css+"</style>"; // for html and css
-    output.contentWindow.eval(js); //  for js
+function run() {
+    // Storing data in Local Storage
+    localStorage.setItem('html_code', html_code.value);
+    localStorage.setItem('css_code', css_code.value);
+    localStorage.setItem('js_code', js_code.value);
+
+    // Executing HTML, CSS & JS code
+    result.contentDocument.body.innerHTML = `<style>${localStorage.css_code}</style>` + localStorage.html_code;
+    result.contentWindow.eval(localStorage.js_code);
 }
+
+// Checking if user is typing anything in input field
+html_code.onkeyup = () => run();
+css_code.onkeyup = () => run();
+js_code.onkeyup = () => run();
+
+// Accessing data stored in Local Storage. To make it more advanced you could check if there is any data stored in Local Storage.
+html_code.value = localStorage.html_code;
+css_code.value = localStorage.css_code;
+js_code.value = localStorage.js_code
